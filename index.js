dd2c25a1c30a5f321e7f3008841c90e115c74b76
Kagzea <erwan.michaudwinkler@gmail.com>
2022-08-15 00:25:38 +0200

    com1

---
+++ index.js
@@ -0,0 +1,9 @@
+function openNav() {
+    document.getElementById("sideNavigation").style.width = "180px";
+    document.getElementById("main").style.marginLeft = "-40px";
+}
+    
+function closeNav() {
+    document.getElementById("sideNavigation").style.width = "0";
+    document.getElementById("main").style.marginLeft = "-40px";
+}
